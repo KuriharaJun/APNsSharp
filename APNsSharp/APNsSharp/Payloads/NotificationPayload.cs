63195c28340be14402c6113487cbf456a42a3fa4
栗原　純 <03s090@live.jp>
2017-12-24 19:53:20 +0900

    Add Notification payload

---
+++ APNsSharp/APNsSharp/Payloads/NotificationPayload.cs
@@ -0,0 +1,114 @@
+﻿//
+// NotificationPayload.cs
+//
+// Author:
+//       kurihara Jun <03s090@live.jp>
+//
+// Copyright (c) 2017 Kuruhara Jun
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.Generic;
+
+namespace APNsSharp.Payloads
+{
+    public class NotificationPayload
+    {
+        /// <summary>
+        /// The sound default.
+        /// </summary>
+        public const string SoundDefault = "default";
+
+        /// <summary>
+        /// Alertに設定可能なkey
+        /// </summary>
+        private static readonly string[] alertKey = new string[] {
+            "title",
+            "body",
+            "title-loc-key",
+            "title-loc-args",
+            "action-loc-key",
+            "loc-key",
+            "loc-args",
+            "launch-image"
+        };
+
+        /// <summary>
+        /// Gets or sets alert key.
+        /// </summary>
+        /// <value>The alert.</value>
+        public IDictionary<string, object> Alert
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the badge key.
+        /// </summary>
+        /// <value>The badge.</value>
+        public int Badge
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the sound key.
+        /// </summary>
+        /// <value>The sound.</value>
+        public string Sound
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the content-avaiable key.
+        /// </summary>
+        /// <value>The content avaiable.</value>
+        public string ContentAvaiable
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Gets or sets category key.
+        /// </summary>
+        /// <value>The category.</value>
+        public string Category
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets thread-id key.
+        /// </summary>
+        /// <value>The thread identifier.</value>
+        public string ThreadId
+        {
+            get;
+            set;
+        }
+
+    }
+}
